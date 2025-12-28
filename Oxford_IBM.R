ba3d69bf389477d1359dd621dc0257d7f80fbee4
Ollie Spacey <ogspacey16@gmail.com>
2025-12-28 12:25:34 +0000

    Created R script to construct an individual-based model for Oxford mistletoes. Loaded data, generated host network, outlined steps for analysis

---
+++ Oxford_IBM.R
@@ -0,0 +1,91 @@
+### Oxford IBM
+### 2025.12.28
+### Oliver G. Spacey
+### Code to generate 
+
+# Pre-amble ---------------------------------------------------------------
+# Clear environment
+rm(list = ls())
+
+# Load packages 
+library(igraph)    # Network construction
+library(tidyverse) # Data wrangling
+
+# Set working directory as required
+
+# Load data --------------------------------------------------------------
+# Load raw census data
+raw_cen_df <- read.csv("Oxford_Trees_Long.csv")
+
+# Load host coordinate data
+hst_coo_df <- read.csv("host_trees_bng_with_coords.csv") %>%
+              rename(TreeID = Name,
+                     lon = easting_bng,
+                     lat = northing_bng)%>%
+              mutate(Index = 1:nrow(read.csv("host_trees_bng_with_coords.csv")))
+
+# Join raw data and coordinates, and add indexing column
+oxf_hst_df <- full_join(raw_cen_df, hst_coo_df) 
+
+# Generate host community ------------------------------------------------
+# Get number of hosts
+n <- nrow(hst_coo_df)
+
+## Weight host network by distance
+# Create empty adjacency matrix
+adj_mat <- matrix(NA, nrow = n, ncol = n)
+
+# Calculate pythagorean distances between hosts and store in adjacency matrix
+for(i in 1:n){
+  i_lon <- hst_coo_df[i,2]
+  i_lat <- hst_coo_df[i,3]
+  for(j in 1:n){
+    j_lon <- hst_coo_df[j,2]
+    j_lat <- hst_coo_df[j,3]
+    adj_mat[i,j] <- sqrt((i_lon - j_lon) ^ 2 + (i_lat - j_lat) ^ 2)
+  }
+}
+
+# Construct network from adjacency matrix
+hst_ntw <- graph_from_adjacency_matrix(adjmatrix = adj_mat,
+                                       mode = "undirected",
+                                       weighted = TRUE)
+
+# Plot host network
+plot(hst_ntw)
+
+# Set parameters ----------------------------------------------------------
+# GO FROM HERE - GET THESE PARAMETERS
+
+
+# Set turnover rate - set to be constant, calculate from felling rate in dataset
+tno_rt <- 
+  
+# Set rate of new infections - number of trees infested in t+1 not infested in t
+inf_rt <- 
+  
+# Set parasite infestation growth rate - mean increase in parasite load, accounting for missed individuals
+gro_rt  <-
+  
+# Homogeneous model
+params <- c(tno_rt, # host turnover rate
+            inf_rt, # rate of new infections
+            gro_rt,  # rate of parasite infestation growth
+            trm_k   # transmission constant trm_probab = k / distance
+)
+
+
+
+# Run model ---------------------------------------------------------------
+# Set initial conditions
+
+# Each time-step
+
+
+
+
+# Output number of parasites on each host
+
+# Plot mean, variation and k parameter
+
+
